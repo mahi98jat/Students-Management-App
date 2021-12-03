7e25b6edd5dce71ea3682bead7352f10f57c6e48
Mahipal Singh Haritwal <mahi98jat@gmail.com>
2021-12-03 14:28:31 +0530

    adding index.js to the application

---
+++ back-end/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const port = process.env.PORT || 3000;
+require("dotenv").config();
+const app = express();
+const connect = require("./src/config/db.js"); // import connection module for connectivity;
+
+// importing students and contests controller here
+const students = require("./src/controllers/students.controller.js");
+const contests = require("./src/controllers/contest.controller.js");
+// importing login controllers here
+const login = require("./src/controllers/login.controller.js");
+
+app.use(express.json()); // it will enable us to work with json data format.
+
+// redirecting routes to students and contest controllers
+app.use("/students", students);
+app.use("/contest", contests);
+app.use("/login", login);
+
+app.listen(port, (req, res) => {
+  connect(); // calling the connect we are connecting with atlas online database.
+  console.log(`Server is responding at port ${port}`);
+});
